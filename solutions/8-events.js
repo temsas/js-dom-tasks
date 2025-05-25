bd06de47acca1cba3373e872b8a53b4d55395b3f
temsas <artemkariakin1187@gmail.com>
2025-05-25 22:19:24 +0300

    Задание 8 выполнено

--- solutions/8-events.js
+++ solutions/8-events.js
@@ -1,5 +1,16 @@
 export default () => {
   // BEGIN
+    let alertCounter = 1
+    const button = document.querySelector('#alert-generator')
+    const alertsContainer = document.querySelector('.alerts')
 
+    button.addEventListener('click', () => {
+        const alertDiv = document.createElement('div')
+
+        alertDiv.className = 'alert alert-primary';
+        alertDiv.textContent = `Alert ${alertCounter++}`;
+
+        alertsContainer.prepend(alertDiv)
+    })
   // END
 };
--- solutions/9-events-in-action.js
+++ solutions/9-events-in-action.js
@@ -1,5 +1,26 @@
+
 export default () => {
   // BEGIN
-  
+    const tabRow = document.querySelectorAll('.row');
+
+    tabRow.forEach((row) => {
+        const tabList = row.querySelectorAll('[role="tab"]');
+
+        const handleClickOnTab = (event) => {
+            const currentTab = event.target;
+            const activeNavLink = row.querySelector('.nav-link.active');
+            const activeTabPane = row.querySelector('.tab-pane.active');
+            const targetTabPane = row.querySelector(currentTab.getAttribute('data-bs-target'));
+
+            activeNavLink.classList.remove('active');
+            activeTabPane.classList.remove('active');
+            targetTabPane.classList.add('active');
+            currentTab.classList.add('active');
+        };
+
+        tabList.forEach((tab) => {
+            tab.addEventListener('click', handleClickOnTab);
+        });
+    });
   // END
 };
