3c10c76565b5c91f2fd1b6383b69b30e9d17ff8d
temsas <artemkariakin1187@gmail.com>
2025-05-25 20:23:26 +0300

    Задание 8 выполнено

--- solutions/6-tree-manipulating.js
+++ solutions/6-tree-manipulating.js
@@ -1,3 +1,17 @@
 // BEGIN
+const prettify = (document) => {
+    const divs = document.querySelectorAll('div')
 
+    divs.forEach(div => {
+        const childNodes = Array.from(div.childNodes)
+        childNodes.forEach(node => {
+            if (node instanceof Text && node.textContent.trim() !== '') {
+                const p = document.createElement('p')
+                p.textContent = node.textContent
+                node.replaceWith(p)
+            }
+        })
+    })
+}
+export default  prettify;
 // END
--- solutions/7-element-manipulating.js
+++ solutions/7-element-manipulating.js
@@ -1,5 +1,20 @@
 import camelCase from 'lodash/camelCase';
 
 // BEGIN
+const camel = (document) => {
+    const allElements = document.body.getElementsByTagName('*')
 
+    for (let i = 0; i < allElements.length; i++) {
+        const element = allElements[i]
+        const classList = Array.from(element.classList)
+
+        classList.forEach(className => {
+            const camelCaseName = camelCase(className)
+            if (camelCaseName !== className) {
+                element.classList.replace(className, camelCaseName)
+            }
+        })
+    }
+}
+export  default camel;
 // END
