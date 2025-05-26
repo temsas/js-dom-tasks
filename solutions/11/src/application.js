827de4d0e4078d8bf89d5cbc758ad42bfff61010
temsas <artemkariakin1187@gmail.com>
2025-05-26 15:42:49 +0300

    Задание 8 выполнено

--- solutions/11/src/application.js
+++ solutions/11/src/application.js
@@ -2,6 +2,47 @@ import 'whatwg-fetch';
 
 export default () => {
   // BEGIN
-  
+    async function fetchSuggestions(url, searchTerm) {
+        try {
+            const searchParams = new URLSearchParams({ search: searchTerm });
+            const fullUrl = new URL(url, window.location.origin)
+            fullUrl.search = searchParams.toString()
+
+            const response = await fetch(fullUrl)
+            const data = await response.json()
+            return data;
+        } catch (error) {
+            return []
+        }
+    }
+    function updateSuggestionsList(inputElement, suggestions) {
+        const autocompleteName = inputElement.dataset.autocompleteName
+        const suggestionsList = document.querySelector(`ul[data-autocomplete-name="${autocompleteName}"]`)
+
+        suggestionsList.innerHTML = '';
+        if (suggestions.length > 0) {
+            suggestions.forEach(suggestion => {
+                const listItem = document.createElement('li')
+                listItem.textContent = suggestion
+                suggestionsList.appendChild(listItem)
+            })
+        } else {
+            const listItem = document.createElement('li')
+            listItem.textContent = 'Nothing'
+            suggestionsList.appendChild(listItem);
+        }
+    }
+
+    const autocompleteInputs = document.querySelectorAll('[data-autocomplete]')
+
+    autocompleteInputs.forEach(inputElement => {
+        inputElement.addEventListener('input', async (event) => {
+            const url = inputElement.dataset.autocomplete
+            const searchTerm = event.target.value;
+
+            const suggestions = await fetchSuggestions(url, searchTerm)
+            updateSuggestionsList(inputElement, suggestions)
+        })
+    })
   // END
 };
--- solutions/5-search.js
+++ solutions/5-search.js
@@ -1,5 +1,29 @@
 export default (document) => {
   // BEGIN
+    const contentDiv = document.querySelector('.content')
+    const titleElement = contentDiv.querySelector('h1')
+    const descriptionElement = contentDiv.querySelector('.description')
+    const articleElements = contentDiv.querySelectorAll('.links > div')
 
+    const title = titleElement ? titleElement.textContent.trim() : ''
+    const description = descriptionElement ? descriptionElement.textContent.trim() : ''
+
+    const items = Array.from(articleElements).map(articleElement => {
+        const titleLink = articleElement.querySelector('h2 a')
+        const descriptionParagraph = articleElement.querySelector('p')
+
+        const articleTitle = titleLink ? titleLink.textContent.trim() : ''
+        const articleDescription = descriptionParagraph ? descriptionParagraph.textContent.trim() : ''
+
+        return {
+            title: articleTitle,
+            description: articleDescription,
+        }
+    })
+    return {
+        title: title,
+        description: description,
+        items: items,
+    }
   // END
 };
