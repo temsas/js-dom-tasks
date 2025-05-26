dd2e854a2f4fb03e23493227b0d3dcec514cfde2
temsas <artemkariakin1187@gmail.com>
2025-05-26 13:47:58 +0300

    Задание 8 выполнено

--- solutions/10/src/application.js
+++ solutions/10/src/application.js
@@ -1,5 +1,24 @@
 import escapeHtml from 'escape-html';
 
 // BEGIN
+export default () => {
+    const form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const formData = new FormData(form)
+        const email = escapeHtml(formData.get('email'))
+        const name = escapeHtml(formData.get('name'))
+        const comment = escapeHtml(formData.get('comment'))
 
+        const successHTML = `
+      <div>
+        <p>Feedback has been sent</p>
+        <div>Email: ${email}</div>
+        <div>Name: ${name}</div>
+        <div>Comment: ${comment}</div>
+      </div>
+    `
+        form.outerHTML = successHTML
+    })
+}
 // END
